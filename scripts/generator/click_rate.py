09087e865c28a83177ec7a50a0044ae33b045053
irfansofyana <irfansofyana0305@gmail.com>
2021-04-05 00:12:38 +0700

    Add click rates

---
+++ scripts/generator/click_rate.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+import random
+
+
+def find_duration_in_minute(d1, d2):
+    d1 = datetime.strptime(d1, "%Y-%m-%d %H")
+    d2 = datetime.strptime(d2, "%Y-%m-%d %H")
+    return (d2 - d1).total_seconds() // 60
+
+
+def generate_click_rates(products, times):
+    click_rates = []
+    NUMBER_OF_GENERATED_CLICK_RATES = 5000
+
+    for _ in range(NUMBER_OF_GENERATED_CLICK_RATES):
+        product_id = random.randint(1, len(products))
+        start_tid = random.randint(1, len(times) - 24)
+        end_tid = start_tid + random.randint(1, 24)
+        duration_in_minute = find_duration_in_minute(
+            times[start_tid]["date"] + " " + str(times[start_tid]["hour"]),
+            times[end_tid]["date"] + " " + str(times[end_tid]["hour"])
+        )
+        number_of_clicks = random.randint(1, 10000)
+        click_rate = number_of_clicks // duration_in_minute
+
+        click_rate_obj = {
+            'product_id': product_id,
+            'start_time_id': start_tid,
+            'end_time_id': end_tid,
+            'duration_in_minute': duration_in_minute,
+            'click_rate': click_rate
+        }
+        click_rates.append(click_rate_obj)
+
+    return click_rates
--- scripts/main.py
+++ scripts/main.py
@@ -2,6 +2,7 @@ from scripts.generator.customer import generate_fake_customers
 from scripts.generator.promo import generate_fake_promos
 from scripts.generator.time import generate_fake_times
 from scripts.generator.product import generate_fake_products
+from scripts.generator.click_rate import generate_click_rates
 
 if __name__ == "__main__":
     customers = generate_fake_customers()
@@ -15,6 +16,11 @@ if __name__ == "__main__":
     times = generate_fake_times()
     for t in times:
         print(t)
+
     products = generate_fake_products()
     for p in products:
         print(p)
+
+    cr = generate_click_rates(products, times)
+    for c in cr:
+        print(c)
