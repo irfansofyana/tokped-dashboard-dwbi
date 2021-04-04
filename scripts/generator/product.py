495d99511e1e2407b83d16739d06ad8c4da7fef0
irfansofyana <irfansofyana0305@gmail.com>
2021-04-04 23:38:00 +0700

    Modify product

--- scripts/generator/product.py
+++ scripts/generator/product.py
@@ -1,26 +1,160 @@
 import random
+from faker import Faker
+
+
+def random_number(r):
+    return random.randint(r[0], r[1])
+
+
+def generate_product_belanja(fake):
+    products = []
+    category_type_and_freq = {
+        "Buku": random_number((10, 20)),
+        "Dapur": random_number((21, 50)),
+        "Elektronik": random_number((200, 500)),
+        "Fashion Anak & Bayi": random_number((51, 100)),
+        "Fashion Muslim": random_number((51, 100)),
+        "Fashion Pria": random_number((51, 100)),
+        "Fashion Wanita": random_number((51, 100)),
+        "Film & Musik": random_number((51, 100)),
+        "Gaming": random_number((21, 50)),
+        "Handphone & Tablet": random_number((21, 50)),
+        "Ibu & Bayi": random_number((10, 20)),
+        "Kamera": random_number((10, 20)),
+        "Kecantikan": random_number((10, 20)),
+        "Kesehatan": random_number((10, 20)),
+        "Komputer & Laptop": random_number((21, 50)),
+        "Logam Mulia": random_number((10, 20)),
+        "Mainan & Hobi": random_number((10, 20)),
+        "Makanan & Minuman": random_number((200, 500)),
+        "Office & Stationery": random_number((21, 50)),
+        "Olahraga": random_number((21, 50)),
+        "Otomotif": random_number((10, 20)),
+        "Perawatan Hewan": random_number((10, 20)),
+        "Perawatan Tubuh": random_number((10, 20)),
+        "Perlengkapan Pesta & Craft": random_number((10, 20)),
+        "Pertukangan": random_number((21, 50)),
+        "Properti": random_number((10, 20)),
+        "Rumah Tangga": random_number((21, 50)),
+        "Tour & Travel": random_number((10, 20)),
+        "Wedding": random_number((10, 20)),
+    }
+
+    for category in category_type_and_freq:
+        for _ in range(category_type_and_freq[category]):
+            name = "product " + fake.first_name_nonbinary()
+            price = random.randint(1, 1000) * 1000
+            weight = random.randint(1, 10)
+
+            product = {
+                'product_name': name,
+                'product_category': category,
+                'product_price': price,
+                'product_weight': weight,
+            }
+
+            products.append(product)
+    return products
+
+
+def generate_product_investasi(fake):
+    products = []
+
+    investasi = [
+        "Apply Kartu Kredit",
+        "Asuransi",
+        "Asuransi Perjalanan",
+        "BRI Ceria",
+        "Dana Impian"
+    ]
+
+    for category in investasi:
+        for _ in range(10):
+            name = "product " + fake.first_name_nonbinary()
+            price = random.randint(1, 5000) * 1000
+            product = {
+                'product_name': name,
+                'product_category': category,
+                'product_price': price,
+                'product_weight': None,
+            }
+            products.append(product)
+
+    return products
+
+
+def generate_product_pajak(fake):
+    products = []
+    pajak = [
+        "E - Samsat",
+        "Pajak PBB",
+        "Penerimaan Negara",
+        "Retribusi",
+    ]
+    for category in pajak:
+        name = "product " + fake.first_name_nonbinary()
+        price = random.randint(100, 3000) * 1000
+        product = {
+            'product_name': name,
+            'product_category': category,
+            'product_price': price,
+            'product_weight': None,
+        }
+        products.append(product)
+    return products
+
+
+def generate_product_pendidikan(fake):
+    products = []
+    pendidikan = [
+        "Belajar",
+        "Kartu Prakerja",
+        "Review Kartu Prakerja",
+    ]
+    for category in pendidikan:
+        for _ in range(20):
+            name = "product " + fake.first_name_nonbinary()
+            price = random.randint(100, 1000) * 1000
+            product = {
+                'product_name': name,
+                'product_category': category,
+                'product_price': price,
+                'product_weight': None,
+            }
+            products.append(product)
+    return products
+
+
+def generate_product_topup(fake):
+    products = []
+    topup = [
+        "token listrik",
+        "pulsa",
+        "emoney"
+    ]
+    for category in topup:
+        for _ in range(30):
+            name = "product " + fake.first_name_nonbinary()
+            price = random.randint(50, 2000) * 1000
+            product = {
+                'product_name': name,
+                'product_category': category,
+                'product_price': price,
+                'product_weight': None,
+            }
+            products.append(product)
+    return products
+
 
 def generate_fake_products():
     products = []
-    
-    #kategori & nama produk sementara dibuat manual dibawah
-    category_type = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l','m','n','o','p','q']
-    product_list_name = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','AA','BB','CC','DD','EE','FF','GG','HH','II','JJ','KK','LL','MM','NN','OO','PP','QQ','RR','SS','TT','UU','VV','WW','XX','YY','ZZ']
+    faker = Faker('id_ID')
+    fake = faker['id_ID']
 
-
-    for produk in range(1000):
-        name = product_list_name[random.randint(0, len(product_list_name)-1)]
-        category = category_type[random.randint(0, len(category_type)-1)]
-        price = random.randint(1,5000)*1000
-        weight = round(random.uniform(0.1,10.0),1)
-
-        product = {
-            'product_name' : name,
-            'product_category' : category,
-            'product_price' : price, #rupiah
-            'product_weight' : weight, # kilogram
-        }
-
-        products.append(product)
+    products += generate_product_belanja(fake)
+    products += generate_product_investasi(fake)
+    products += generate_product_pajak(fake)
+    products += generate_product_topup(fake)
+    products += generate_product_pendidikan(fake)
 
     return products
--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,7 @@
 from scripts.generator.customer import generate_fake_customers
 from scripts.generator.promo import generate_fake_promos
 from scripts.generator.time import generate_fake_times
+from scripts.generator.product import generate_fake_products
 
 if __name__ == "__main__":
     customers = generate_fake_customers()
@@ -14,3 +15,6 @@ if __name__ == "__main__":
     times = generate_fake_times()
     for t in times:
         print(t)
+    products = generate_fake_products()
+    for p in products:
+        print(p)
